7d636c1bd48826a8465f830124a5379085c286f5
Pierre BATIGNES <batignesp@gmail.com>
2020-04-23 17:24:49 +0200

    Collegue 2

---
+++ src/app/mock/collegues.mock.ts
@@ -0,0 +1,5 @@
+import { Collegue } from '../models/collegue';
+
+export class collegueMock {
+    col = new Collegue('M04', 'Batignes', 'Pierre', 'email@email.fr', new Date('1995-12-20'), 'photo.jpg');
+}
